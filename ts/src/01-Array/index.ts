447a6d72f54b8c13f866a22f30d6fdefe0725104
Potter <75838259@qq.com>
2021-10-31 09:28:21 +0800

    TS部分:配置好学习环境

---
+++ ts/src/01-Array/index.ts
@@ -0,0 +1,7 @@
+
+function sum(num1: number, num2: number) {
+    debugger
+    return num1 + num2;
+}
+
+export default { sum }
---
+++ ts/src/index.ts
@@ -0,0 +1,6 @@
+
+import Arr from "./01-Array/index"
+
+console.log("hello:" + Arr.sum(1, 2));
+
+export { }
