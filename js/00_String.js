280795e0679dd4250629722a1c9e70f0deb356ad
Potter <75838259@qq.com>
2021-12-13 11:16:55 +0800

    新增：按需引入组件正则转换代码

--- js/00_String.js
+++ js/00_String.js
@@ -4,3 +4,15 @@ String.isNullOrEmpty = function () {
 
 let a = null;
 console.log(String.isNullOrEmpty(a));
+
+
+String.prototype.trim = function (char = "\\s") {
+  let reg = new RegExp(`(^${char}*)|(${char}*$)`, "g");
+  console.log(reg);
+  let result = this.replace(reg, "");
+  return result;
+};
+
+let content = "''abc'";
+content = content.trim("'");
+console.log(content);
