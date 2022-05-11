a5f6c6e419303b8f8ef68aea14fc1fdd8c6b1abd
Potter <75838259@qq.com>
2022-05-11 10:13:21 +0800

    feat: add String example

--- js/00-String/trim.js
+++ js/00-String/trim.js
@@ -1,16 +1,16 @@
 String.isNullOrEmpty = function () {
-    return this == null || this == "";
-  };
+	return this == null || this == "";
+};
 
 let a = null;
 console.log(String.isNullOrEmpty(a));
 
 
 String.prototype.trim = function (char = "\\s") {
-  let reg = new RegExp(`(^${char}*)|(${char}*$)`, "g");
-  console.log(reg);
-  let result = this.replace(reg, "");
-  return result;
+	let reg = new RegExp(`(^${char}*)|(${char}*$)`, "g");
+	console.log(reg);
+	let result = this.replace(reg, "");
+	return result;
 };
 
 let content = "''abc'";
