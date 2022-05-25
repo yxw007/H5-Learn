2462497b700be7e7d497b6b76b73168987f1f9e6
Potter <75838259@qq.com>
2022-05-25 23:07:29 +0800

    add: core-js test promise

--- js/07-promise/test/04.then-promise.js
+++ js/07-promise/test/04.then-promise.js
@@ -1,21 +1,26 @@
 
-const Promise = require("../Promise");
+// const Promise = require("../Promise");
 // const Promise = require("../4.promise");
+
+//! 说明：移植被元素的Promise 影响，可以采用直接把原始的promise delete 掉, 不就可以应用core-js中的promise了
+delete global.Promise;
+
+const Promise2 = require("core-js-pure/actual/promise");
 
 //! then 返回promise
 
-const p = new Promise((resolve, reject) => {
+const p = new Promise2((resolve, reject) => {
 	resolve("success");
 });
 
 //! 循环引用
-/* const p2 = p.then((val) => {
+const p2 = p.then((val) => {
 	return p2;
 }, error => {
 	console.log("p2-error:", error);
-}) */
+})
 
-const p2 = p.then((val) => {
+/* const p2 = p.then((val) => {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
 			resolve("xxxx");
@@ -25,7 +30,7 @@ const p2 = p.then((val) => {
 	console.log("p2.then-success:", val);
 }, err => {
 	console.log("p2.then-fail:", err);
-});
+}); */
 
 
 /* const p = new Promise((resolve, reject) => {
