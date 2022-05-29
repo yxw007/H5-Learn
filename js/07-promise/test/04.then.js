6b20f7fea81108e1f57512471d0230fbba191bcc
Potter <75838259@qq.com>
2022-05-29 15:08:26 +0800

    make promise perfect: then().then(...)

--- js/07-promise/test/04.then.js
+++ js/07-promise/test/04.then.js
@@ -1,8 +1,12 @@
 const Promise = require("../Promise");
+// const Promise = require("../4.promise");
 
-const p = new Promise((resolve, reject) => {
+// delete global.Promise;
+// const Promise = require("core-js-pure/actual/promise");
+
+/* const p = new Promise((resolve, reject) => {
 	resolve("success");
-});
+}); */
 
 //! 说明：如果then onResolve 回调不返回值，下一个then的onResolve 收到的val值就是undefined
 /* p.then((val) => {
@@ -12,12 +16,12 @@ const p = new Promise((resolve, reject) => {
 }); */
 
 //! 说明：如果then onRsolve 回调返回普通值，则传递给下一个then的onResolve 收到的val参数
-p.then((val) => {
+/* p.then((val) => {
 	console.log("then:", val);
 	return val;
 }).then((val2) => {
 	console.log("then2:", val2);
-});
+}); */
 
 //! 说明：如果then onRsolve 回调返回Promise，会先解析promise的值，然后根据值再往后传递(reject和throw往onReject和catch传递，否则就往then传递)
 /*p.then((val) => {
@@ -55,3 +59,14 @@ p.then((val) => {
 }).catch(error => {
 	console.error("catch:", error);
 }); */
+
+const p = new Promise((resolve, reject) => {
+	reject("fail");
+});
+
+//! 说明：如果当前then什么都不做，就会把对应的值传递给下一个存在有回调的then
+p.then().then((val) => {
+	console.log("then.success:", val);
+}, (error) => {
+	console.log("then.error:", error);
+});
