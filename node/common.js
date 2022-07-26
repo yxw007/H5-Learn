055395eaa6683b6a86c1e0374fc898d1096138a9
Potter <75838259@qq.com>
2022-07-26 23:31:34 +0800

    add:fs copyfile example

--- node/common.js
+++ node/common.js
@@ -5,3 +5,10 @@ exports.autoRun = function (name, executor) {
 	executor();
 	console.log(`-------------------------------------------------------------------------\n`);
 }
+
+exports.autoRunPromise = async function (name, executor) {
+	console.log(name);
+	console.log(`-------------------------------------------------------------------------`);
+	await new Promise((resolve) => { executor(resolve) });
+	console.log(`-------------------------------------------------------------------------\n`);
+}
---
+++ node/stream/test/fsCopy.js
@@ -0,0 +1,150 @@
+/**
+ * 创建日期: 2022-07-26
+ * 文件名称：fsCopy.js
+ * 创建作者：Potter
+ * 开发版本：1.0.0
+ * 相关说明：利用最原始的方式分片copy文件内容至另外一个文件
+ */
+
+const fs = require("fs");
+const path = require("path");
+const { autoRunPromise } = require("../../common");
+
+const sourcePath = path.join(__dirname, "../res/Source.txt");
+const targetPath = path.join(__dirname, "../res/Target.txt");
+
+(async () => {
+	await autoRunPromise("1.最原始的方式[整文件]copy文件", (next) => {
+		fs.open(sourcePath, "r", (error, fd) => {
+			if (error) {
+				console.error("open source error:", error);
+				return;
+			}
+
+			fs.read(fd, (error2, bytesRead, buffer) => {
+				if (error2) {
+					console.error("read source error:", error);
+					return;
+				}
+
+				//! 说明：bytesRead 实际读取到的字节数
+				console.log("read source bytesRead:", bytesRead);
+				console.log("read source buffer:", buffer);
+				console.log("read source buffer.len:", buffer.length); //16384 => 16k
+
+				fs.open(targetPath, "w", (wError, wFd) => {
+					if (wError) {
+						console.error("open target error:", wError);
+						return;
+					}
+
+					//! 说明: 如果不传有效的bytesRead值，当source文件字节数不满16k时,会把16k的默认数据也写进目标文件中，所以此处需要传byteRead
+					fs.write(wFd, buffer, 0, bytesRead, (wError2) => {
+						if (wError2) {
+							console.error("write target error2:", wError2);
+						}
+
+						console.log("write target success !");
+
+						//1.close target file
+						fs.close(wFd);
+
+						//2.close source file
+						fs.close(fd);
+
+						next();
+					});
+				});
+			});
+		});
+	});
+
+
+	await autoRunPromise("2.最原始的方式[分片]copy文件", (next) => {
+
+		function closeFile(fd) {
+			fs.close(fd);
+		}
+
+		fs.open(sourcePath, "r", (error, fd) => {
+			if (error) {
+				console.error("open source error:", error);
+				closeFile(fd);
+				return;
+			}
+
+			fs.open(targetPath, "w", (wError, wFd) => {
+				if (wError) {
+					console.error("open target error:", wError);
+					closeFile(wFd);
+					closeFile(fd);
+					return;
+				}
+
+				let cacheSize = 3;
+				let cacheBuff = Buffer.alloc(cacheSize);
+				let offset = 0;
+
+				function copy(callback) {
+
+					console.log("------------------------------");
+
+					fs.read(fd, cacheBuff, 0, cacheBuff.length, offset, (error2, bytesRead, buffer) => {
+						if (error2) {
+							console.error("read source error:", error);
+							callback(false, "read source error:" + error);
+							return;
+						}
+
+						if (bytesRead === 0) {
+							callback(true);
+							return;
+						}
+
+						//! 说明：bytesRead 实际读取到的字节数
+						console.log("read source bytesRead:", bytesRead);
+						console.log("read source buffer:", buffer);
+						console.log("read source buffer.len:", buffer.length); //16384 => 16k
+
+						let lastOffset = offset;
+						offset += bytesRead;
+
+						//! 说明：最后一次不够cacheSize，所以值截取出有效的数据
+						if (bytesRead < cacheBuff.length) {
+							// cacheBuff = cacheBuff.slice(0, bytesRead);
+							cacheBuff = Uint8Array.prototype.slice.call(cacheBuff, 0, bytesRead)
+						}
+
+						//! 说明: 如果不传有效的bytesRead值，当source文件字节数不满16k时,会把16k的默认数据也写进目标文件中，所以此处需要传byteRead
+						fs.write(wFd, cacheBuff, 0, bytesRead, lastOffset, (wError2) => {
+							if (wError2) {
+								console.error("write target error2:", wError2);
+								callback(false, "write target error2:" + wError2);
+								return;
+							}
+
+							console.log(`copy content ----[${cacheBuff.toString()}]----`);
+							console.log("write target success !");
+							copy(callback);
+						});
+					});
+				}
+
+				copy((success, message) => {
+					closeFile(wFd);
+					closeFile(fd);
+					next();
+
+					if (!success) {
+						console.error("copy fail! ", message);
+						return;
+					}
+
+					console.log("copy success !");
+				});
+			});
+		});
+	});
+
+})()
+
