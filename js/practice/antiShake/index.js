442d46e4fe2e1d594dd77f3eae4ce13df8585a34
Potter <75838259@qq.com>
2022-09-19 18:02:51 +0800

    fix: throttle not update last call params

--- js/practice/antiShake/index.js
+++ js/practice/antiShake/index.js
@@ -2,14 +2,14 @@ const { debounce, throttleStamp, throttleTimer } = require("./antiShake");
 
 let obj = {};
 let callback = (one, two) => {
-  console.log("tick", one, two);
+	console.log("tick", one, two);
 };
 let time = 5000;
 let deb = debounce(obj, callback, time);
 let thStamp = throttleStamp(callback, time);
-let thTimer = throttleTimer(callback, time);
+let thTimer = throttleTimer(callback, time);/*  */
 
 setInterval(() => {
-  console.log("interval");
-  thTimer(123);
+	console.log("interval");
+	thTimer(123);
 }, 1000);
