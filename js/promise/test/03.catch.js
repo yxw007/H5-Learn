9b95b1a07d7ba020e9f46c96fa051317f2e823bc
Potter <75838259@qq.com>
2023-02-23 14:17:45 +0800

    add: comment

--- js/promise/test/03.catch.js
+++ js/promise/test/03.catch.js
@@ -1,57 +1,72 @@
-// const Promise = require("../Promise");
+const Promise = require("../Promise");
 
 // delete global.Promise;
 // const Promise = require("core-js-pure/actual/promise");
 
 /* const p = new Promise((resolve, reject) => {
-	reject("fail");
+  reject("fail");
 }); */
 
 //! 说明：如果有error处理，就不会继续往后抛异常，catch也就不会执行
-/* p.then(null, error => {
-	console.error("error:", error);
-}).catch(error => {
-	console.log("catch:", error);
-});
- */
+/* p.then(null, (error) => {
+  console.error("error:", error);
+}).catch((error) => {
+  console.log("catch:", error);
+}); */
+
 //! 说明：如果error回调为空，那么抛出的异常就会走到catch回调
-/* p.then(null, null).catch(error => {
-	console.log("catch:", error);
+/* p.then(null, null).catch((error) => {
+  console.log("catch:", error);
 }); */
 
 //! 说明：此种情况和上一种情况输出一样
-/* p.catch(error => {
-	console.log("catch:", error);
+/* p.catch((error) => {
+  console.log("catch:", error);
 }); */
 
 //! 如果p既不then处理error回调，也不catch捕获异常，底层就会直接把异常抛出,
 /* const p = new Promise((resolve, reject) => {
-	throw "xxx"
-});
- */
+  throw "xxx";
+}); */
 
 //! 说明：then onReject 如何返回不是throw 或 promise reject,返回值都会传递给下一个then 当作onResolve 参数
 //-------------------------------------------------------------------------
 /* const p = new Promise((resolve, reject) => {
-	reject("fail");
-}).then(null, error => {
-	return error;
-}).then(val => {
-	console.log("then2-success:", val);
-}, error => {
-	console.log("then2-erro:", error);
-});
- */
+  reject("fail");
+})
+  .then(null, (error) => {
+    return error;
+  })
+  .then(
+    (val) => {
+      console.log("then2-success:", val);
+    },
+    (error) => {
+      console.log("then2-erro:", error);
+    }
+  ); */
 
-/* const p = new Promise((resolve, reject) => {
-	reject("fail");
-}).then(null, error => {
-	return new Promise((resolve, reject) => reject("new Promise ->reject"));
-}).then(val => {
-	console.log("then2-success:", val);
-}, error => {
-	console.log("then2-erro:", error);
-}); */
+//TODO: 待搞懂这个问题的流程
+const p = new Promise((resolve, reject) => {
+  reject("fail");
+})
+  .then(null, (error) => {
+    //p1
+    return new Promise((resolve, reject) => {
+      //p3
+      reject("new Promise ->reject");
+    });
+  })
+  .then(
+    //p2
+    (val) => {
+      console.log("then2-success:", val);
+    },
+    (error) => {
+      console.log("then2-erro:", error);
+    }
+  );
+//-------------------------------------------------------------------------
 
 //-------------------------------------------------------------------------
 
@@ -72,7 +87,7 @@
 	})
 } */
 
-async function register() {
+/* async function register() {
 	const ret = { succes: true, message: "" };
 	return new Promise((resolve, reject) => {
 		//! 说明：异步抛出的异常，promise.catch 是捕获不到的
@@ -97,6 +112,6 @@ async function goLogin() {
 (async () => {
 	let result = await goLogin();
 	console.log("result:", result);
-})();
+})(); */
 
 //-------------------------------------------------------------------------
