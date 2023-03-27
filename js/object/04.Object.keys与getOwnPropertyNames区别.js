de5588547792c9f9ecde72e3c048c36e6c30e929
Potter <75838259@qq.com>
2023-03-27 14:17:47 +0800

    add: js example

---
+++ js/object/04.Object.keys与getOwnPropertyNames区别.js
@@ -0,0 +1,43 @@
+class Animal {
+  constructor(age) {
+    this.age = age;
+  }
+}
+
+class People extends Animal {
+  constructor(name, age) {
+    super(age);
+    this.name = name;
+    this.play = function () {};
+  }
+}
+
+People.prototype.work = function () {};
+
+let p = new People("pt", 18);
+Object.defineProperty(p, "sex", {
+  value: "boy",
+  enumerable: false,
+});
+
+//! 获取可枚举的属性key
+console.log(Object.keys(p));
+//! 获取所有属性key，包括可枚举和不可枚举的属性, 不会获取原型上的属性
+console.log(Object.getOwnPropertyNames(p));
+
+//! enties 与keys类似
+for (const [key, value] of Object.entries(p)) {
+  console.log(`${key}: ${value}`);
+}
+
+console.log("--------------");
+//! for in 会获取可枚举的当前对象和原型链上的属性，可通过hasOwnProperty排除掉原型连上的属性和方法
+for (key in p) {
+  console.log(`${key}: ${p[key]}`);
+}
+console.log("--------------");
+for (key in p) {
+  if (p.hasOwnProperty(key)) {
+    console.log(`${key}: ${p[key]}`);
+  }
+}
