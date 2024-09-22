b66b900091b888f2fe9026aeb59c0155e6f152e2
Potter <aa4790139@gmail.com>
2024-09-22 13:42:26 +0800

    feat: ts 动态类型demo

--- ts/src/13-动态类型/index.ts
+++ ts/src/13-动态类型/index.ts
@@ -5,54 +5,61 @@
 const amazon = {
 	"a-amazon": "amazon",
 	"b-amazon": "b2"
-}
+} as const;
+
 const azure = {
 	"a-azure": "a1",
 	"b-azure": "b2"
-}
+} as const;
+
 const google = {
 	"a-google": "a1",
 	"b-google": "b2"
-}
+} as const;
 
 type Engines = "amazon" | "azure" | "google"
 
-export type LanguageMap = {
+export type targetLanguageMapNames = {
 	"amazon": keyof typeof amazon,
 	"azure": keyof typeof azure,
 	"google": keyof typeof google,
 }
 
-type KeysOf<T extends keyof LanguageMap> = LanguageMap[T];
+export type ValuesOf<T> = T[keyof T];
+type targetLanguageMapValues = {
+	amazon: ValuesOf<typeof amazon>;
+	azure: ValuesOf<typeof azure>;
+	google: ValuesOf<typeof google>;
+};
 
-export type TranslateOptions<E extends Engines> = {
-	from?: KeysOf<E> | "auto";
-	to: KeysOf<E>;
-	engine?: E;
+// type AL = targetLanguageMapValues["amazon"];
+// let al: AL = "";
+
+type KeysOfTarget<T extends keyof targetLanguageMapNames> = targetLanguageMapNames[T];
+
+export type TranslateOptions<T extends Engines> = {
+	from?: KeysOfTarget<T> | "auto";
+	to: KeysOfTarget<T> | targetLanguageMapValues[T];
+	engine?: T;
 }
 
 export type EngineTranslateOptions<T extends Engines> = Omit<TranslateOptions<T>, "engine">;
 
 export type Engine = {
 	name: string;
-	isValidOriginLanguage: (originLanguage: any) => boolean;
-	isValidTargetLanguage: (targetLanguage: any) => boolean;
 	translate: <T extends Engines>(text: string | string[], opts: EngineTranslateOptions<T>) => Promise<string[]>;
 };
 
 class Translator {
-	private engines: Map<string, Engine>;
 	constructor() {
-		this.engines = new Map<string, Engine>();
 	}
 	translate<T extends Engines>(options: TranslateOptions<T>) {
 	}
 }
 
 let translator = new Translator();
-
 translator.translate({
 	engine: "amazon",
-	to: "a-amazon"
+	to: ""
 })
 
